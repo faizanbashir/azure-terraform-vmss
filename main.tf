b191d421e1bcb1d83257dd8cc3afa84515f83d23
Faizan Bashir <faizann.teli@honeywell.com>
2021-04-18 13:18:29 +0530

    Moved terraform and azure config from main to backend.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    azurerm = "~>2.24.0"
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
 locals {
   resource_group_name = "${var.application}-${var.environment}"
   vnet_name           = "${var.application}-${var.environment}-vnet"
